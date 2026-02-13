8da1a536111636bc4ddd2d7685b44c242ae6e585
Tan Ngo <tannm@upp-technology.com>
2026-02-13 15:19:57 +0100

    remove test

--- test/test.py
+++
@@ -1,9 +0,0 @@
-with open("metadata", "rb") as f:
-    data = f.read()
-
-try:
-    data.decode("utf-8")
-    print("File is valid UTF-8.")
-except UnicodeDecodeError as e:
-    print(f"Error decoding file: {e}")
-
